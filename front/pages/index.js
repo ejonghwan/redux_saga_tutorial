bee127543ff361e2ef5149b401b5a6f61785e624
ejonghwan <sun87-1@daum.net>
2021-08-30 00:58:17 +0900

    a

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+
+
 
 const Index = () => {
+
+    const [txt, setTxt] = useState('') 
+    const [txt2, setTxt2] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        datafn(txt)
+    }
+
+    const handleCahnge = (e) => {
+        const { value } = e.target
+        setTxt(value)
+    }
+
+
+    const datafn = async(data) => {
+        // console.log(data)
+        const result = await axios.post('http://localhost:3060/test', {he: data})
+        console.log(result.data.he)
+        setTxt2(result.data.he)
+        return result.data.he
+    } 
+
+    // const dataman = datafn()
+    // console.log(dataman)
+
     return (
         <div>
-            asdsad
+            <form onSubmit={handleSubmit}>
+                <input value={txt} name="hoho" onChange={handleCahnge} />
+                <button>submit</button>
+            </form>
+            {txt}
+            <br />
+            data: {txt2}
         </div>
     )
 }
