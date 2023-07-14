db94972f00e392355550ab40ca76329b9cd61531
ejonghwan <sun87-1@daum.net>
2023-07-14 11:09:42 +0900

    리덕스 데브툴즈 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux'; //compose 추가
 import reducer from '../reducers';
-
+import { composeWithDevTools } from '@redux-devtools/extension'; //추가
 //saga
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
+
+
 const createSaga = createSagaMiddleware();
+const middleware = [createSaga] // 추가
+const enhancer = process.env.NODE_ENV === 'production'
+  ? compose(applyMiddleware(...middleware)) : composeWithDevTools(applyMiddleware(...middleware)) //추가
 
 
-
-const store = createStore(reducer, applyMiddleware(createSaga))
+const store = createStore(reducer, enhancer) // 수정
 createSaga.run(rootSaga)
 
 export default store;
